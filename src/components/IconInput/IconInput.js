f1fb0f44307cacc482b858be21ec2e19676a968e
GK <combondrn@gmail.com>
2021-04-04 15:13:46 -0700

    Started IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  return (
+    <Wrapper>
+      <InputLabel>
+        <VisuallyHidden>Input for stuff here!</VisuallyHidden>
+        <TextInput />
+        <IconWrapper>
+          <Icon id={icon} size={16} strokeWidth={1} />
+        </IconWrapper>
+      </InputLabel>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const InputLabel = styled.label``;
+
+const TextInput = styled.input`
+  padding-left: 32px;
+  /* border:none; */
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  height: 16px;
+  margin: auto;
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,64 +1,64 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <>
-      <FakeSelect>
-        <HiddenSelect value={value} onChange={onChange}>
-          {children}
-        </HiddenSelect>
-        {displayedValue}
-        <IconWrapper style={{ "--size": 24 + 'px' }}>
-          <Icon id="chevron-down" size={24} strokeWidth={2} />
-        </IconWrapper>
-      </FakeSelect>
-      <select value={value} onChange={onChange}>
+    <Wrapper>
+      <HiddenSelect value={value} onChange={onChange}>
         {children}
-      </select>
-    </>
+      </HiddenSelect>
+      {displayedValue}
+      <IconWrapper style={{ "--size": 24 + "px" }}>
+        <Icon id="chevron-down" size={24} strokeWidth={2} />
+      </IconWrapper>
+    </Wrapper>
   );
 };
 
-const FakeSelect = styled.button`
-display:block;
- position: relative;
- background-color: ${COLORS.transparentGray15};
- padding: 12px 52px 12px 16px; 
- border-radius: 8px;
- font-size: ${16 / 16}rem;
- color: ${COLORS.gray700};
- border:none;
-
- &:hover {
- color: ${COLORS.black};
- }
+const HiddenSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
 `;
 
-const HiddenSelect = styled.select`
- position: absolute;
- top: 0;
- left:0;
- width: 100%;
- height: 100%;
- opacity:0.0;
+const Wrapper = styled.div`
+  position: relative;
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 52px 12px 16px;
+  border-radius: 8px;
+  font-size: ${16 / 16}rem;
+  color: ${COLORS.gray700};
+  border: none;
+  width: max-content;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus-within {
+    outline: 1px dotted gray;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
 `;
 
 const IconWrapper = styled.div`
-position:absolute;
-right:10px;
-top:0;
-bottom:0;
-width: var(--size);
-height: var(--size);
-margin:auto;
-pointer-events:none;
+  position: absolute;
+  right: 10px;
+  top: 0;
+  bottom: 0;
+  width: var(--size);
+  height: var(--size);
+  margin: auto;
+  pointer-events: none;
 `;
 
 export default Select;
