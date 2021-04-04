3ca2f0f84675e462197f038920623e5ec5fd0157
GK <combondrn@gmail.com>
2021-04-04 11:59:11 -0700

    First pass on ProgressBar and Select

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,75 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const ProgressBar = ({ value, size = "medium" }) => {
+  const VAL_MIN = 0,
+    VAL_MAX = 100;
+  if (value < VAL_MIN || VAL_MAX < value) {
+    return "INVALID VALUE"
+  }
+  var valRange = VAL_MAX - VAL_MIN,
+    percentComplete = Math.round(((value - VAL_MIN) / valRange) * 100);
+
+  var Component;
+  if (size === "small") {
+    Component = WrapperSmall;
+  } else if (size === "medium") {
+    Component = WrapperMedium;
+  } else {
+    Component = ({ children }) => <WrapperLarge><WrapperPadding>{children}</WrapperPadding></WrapperLarge>;;
+  }
+  return (
+    <Component
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin={VAL_MIN}
+      aria-valuemax={VAL_MAX}
+    >
+      <Bar percentComplete={percentComplete}>
+        <VisuallyHidden>{percentComplete}%</VisuallyHidden>
+      </Bar>
+    </Component>
+  );
 };
+
+const WrapperBase = styled.div`
+position:relative;
+background-color: ${COLORS.transparentGray15};
+box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+/* trim corners when progress bar is near full */
+overflow:hidden;
+`;
+
+const WrapperSmall = styled(WrapperBase)`
+height: 8px;
+border-radius: 4px;
+`;
+const WrapperMedium = styled(WrapperBase)`
+height: 12px;
+border-radius: 4px;
+`;
+const WrapperLarge = styled(WrapperBase)`
+height: 24px;
+border-radius: 8px;
+`;
+
+const WrapperPadding = styled.div`
+position:absolute;
+top:4px;
+bottom:4px;
+left:4px;
+right:4px;
+border-radius: 4px;
+overflow: hidden;
+`;
+
+const Bar = styled.div`
+position: absolute;
+top:0;
+bottom: 0;
+left:0;
+width: ${p => p.percentComplete}%;
+background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,56 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+      <FakeSelect>
+        <HiddenSelect value={value} onChange={onChange}>
+          {children}
+        </HiddenSelect>
+        {displayedValue}
+        <IconWrapper style={{ "--size": 24 + 'px' }}>
+          <Icon id="chevron-down" size={24} strokeWidth={2} />
+        </IconWrapper>
+      </FakeSelect>
+      <select value={value} onChange={onChange}>
+        {children}
+      </select>
+    </>
   );
 };
+
+const FakeSelect = styled.button`
+display:block;
+ position: relative;
+ background-color: ${COLORS.transparentGray15};
+ padding: 12px 52px 12px 16px; 
+ border-radius: 8px;
+ font-size: ${16 / 16}rem;
+ color: ${COLORS.gray700};
+ border:none;
+
+ &:hover {
+ color: ${COLORS.black};
+ }
+`;
+
+const HiddenSelect = styled.select`
+ position: absolute;
+ top: 0;
+ left:0;
+ width: 100%;
+ height: 100%;
+ opacity:0.0;
+`;
+
+const IconWrapper = styled.div`
+position:absolute;
+right:10px;
+top:0;
+bottom:0;
+width: var(--size);
+height: var(--size);
+margin:auto;
+pointer-events:none;
+`;
 
 export default Select;
