97d638c30210c0e7c240e7cc38d3ddee38891753
GK <combondrn@gmail.com>
2021-04-04 21:32:14 -0700

    Finish IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,38 +6,88 @@ import { COLORS } from "../../constants";
 import Icon from "../Icon";
 import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+const SIZES = {
+  small: {
+    "--paddingVert": 4 + "px",
+    "--paddingLeft": 24 + "px",
+    "--borderWidth": 1 + "px",
+    "--fontSize": 14 + "px",
+  },
+  large: {
+    "--paddingVert": 8 + "px",
+    "--paddingLeft": 36 + "px",
+    "--borderWidth": 2 + "px",
+    "--fontSize": 18 + "px",
+  },
+};
+
+const IconInput = ({
+  label,
+  icon,
+  width = 250,
+  size,
+  placeholder,
+  ...delegated
+}) => {
+  var iconSize = size === "small" ? 16 : 24;
+
+  var style = SIZES[size];
+  if (!style) {
+    throw new Error("Invalid size specified");
+  }
+
   return (
-    <Wrapper>
-      <InputLabel>
-        <VisuallyHidden>Input for stuff here!</VisuallyHidden>
-        <TextInput />
-        <IconWrapper>
-          <Icon id={icon} size={16} strokeWidth={1} />
-        </IconWrapper>
-      </InputLabel>
-    </Wrapper>
+    <InputLabel>
+      <VisuallyHidden>Input for stuff here!</VisuallyHidden>
+      <TextInput
+        style={{ ...style, "--width": width + "px" }}
+        placeholder={placeholder}
+        {...delegated}
+      />
+      <IconWrapper style={{ "--iconSize": iconSize + "px" }}>
+        <Icon id={icon} size={iconSize} strokeWidth={2} />
+      </IconWrapper>
+    </InputLabel>
   );
 };
 
-const Wrapper = styled.div`
+const InputLabel = styled.label`
   position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
 `;
 
-const InputLabel = styled.label``;
-
 const TextInput = styled.input`
-  padding-left: 32px;
-  /* border:none; */
+  border: none;
+  outline-offset: 2px;
+  color: inherit;
+  font-weight: 700;
+
+  padding-top: var(--paddingVert);
+  padding-bottom: var(--paddingVert);
+  padding-left: var(--paddingLeft);
+  border-bottom: var(--borderWidth) solid ${COLORS.black};
+  font-size: var(--fontSize);
+  width: var(--width);
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
 `;
 
 const IconWrapper = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
-  left: 0;
-  height: 16px;
+  left: 2px;
+  height: var(--iconSize);
   margin: auto;
+  /* Not necessary, but helpful reminder that intentionally inheriting color */
+  color: inherit;
 `;
 
 export default IconInput;
